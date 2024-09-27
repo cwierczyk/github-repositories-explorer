e42998a44de44f73caa4f07862ca9db7683db322
Mateusz Swierczynski <mateusz.swierczynski@binarapps.com>
2024-09-27 22:45:26 +0200

    Basic setup of Vite, React, TypeScript and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      },
+    },
+    files: ['**/*.{ts,tsx}'],
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        { props: 'never', children: 'never', propElementValues: 'always' },
+      ],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['^@/'],
+            ['^\\./', '^\\.\\./'],
+            ['^.+\\.css$'],
+          ],
+        },
+      ],
+    },
+  },
+);
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+
+export default {
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+};
